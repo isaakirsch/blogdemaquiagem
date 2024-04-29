06461e0a23dea965c208bbc71a55abece5a4a48c
isaakirsch <152524005+isaakirsch@users.noreply.github.com>
2024-04-29 01:09:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-// Inicializa o carrossel
 var myCarousel = new bootstrap.Carousel(document.getElementById('carouselExampleFade'), {
-    interval: 2000, // Intervalo entre os slides (opcional)
-    wrap: true // Define se o carrossel deve voltar ao primeiro slide após o último (opcional)
+    interval: 2000,
+    wrap: true 
   });
   
